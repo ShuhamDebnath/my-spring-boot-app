a24f618b6b03b264e7612b9b1b8b54b25fb23dbb
Shuham Debanth <shuhamdebnath55@gmail.com>
2025-12-27 23:05:08 +0530

    Initial commit

---
+++ src/main/kotlin/com/shuham/spring_boot_crash_course/SpringBootCrashCourseApplication.kt
@@ -0,0 +1,14 @@
+package com.shuham.spring_boot_crash_course
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootCrashCourseApplication
+
+
+//Using generated security password: 78e508f6-9771-41e7-9fd4-db2e54f677a6
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootCrashCourseApplication>(*args)
+}
